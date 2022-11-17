9627fc738d9cbf34deb30befbd592ec4ca3b3f06
0xdcarns <dillon.carns@gmail.com>
2022-11-17 14:20:48 -0500

    windows init

--- cmd/root.go
+++ cmd/root.go
@@ -5,10 +5,8 @@ Copyright © 2022 Netmaker Team <info@netmaker.io>
 package cmd
 
 import (
-	"log"
 	"os"
 	"runtime"
-	"strconv"
 
 	"github.com/google/uuid"
 	"github.com/gravitl/netclient/config"
@@ -139,21 +137,5 @@ func checkConfig() {
 	}
 	if fail {
 		logger.FatalLog("configuration is invalid, fix before proceeding")
-	}
-}
-
-// checkUID - Checks to make sure user has root privileges
-func checkUID() {
-	// start our application
-	out, err := ncutils.RunCmd("id -u", true)
-	if err != nil {
-		log.Fatal(out, err)
-	}
-	id, err := strconv.Atoi(string(out[:len(out)-1]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	if id != 0 {
-		log.Fatal("This program must be run with elevated privileges. Please re-run with sudo or as root.")
 	}
 }
