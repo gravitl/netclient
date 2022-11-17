b2b81b717cc39273beb15db47f404462222a8dd9
Matthew R. Kasun <mkasun@nusak.ca>
2022-11-17 15:42:10 -0500

    check for root without using os.Exec

--- cmd/root_unix.go
+++ cmd/root_unix.go
@@ -5,23 +5,17 @@ package cmd
 
 import (
 	"log"
-	"strconv"
-
-	"github.com/gravitl/netclient/ncutils"
+	"os/user"
 )
 
 // checkUID - Checks to make sure user has root privileges
 func checkUID() {
 	// start our application
-	out, err := ncutils.RunCmd("id -u", true)
-	if err != nil {
-		log.Fatal(out, err)
-	}
-	id, err := strconv.Atoi(string(out[:len(out)-1]))
+	user, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if id != 0 {
+	if user.Username != "root" {
 		log.Fatal("This program must be run with elevated privileges. Please re-run with sudo or as root.")
 	}
 }
