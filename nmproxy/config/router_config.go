4af8f006a6bdefe72855e1345864cb7ff261c950
Abhishek Kondur <abhi281342@gmail.com>
2022-12-14 01:14:23 +0530

    egress test

--- nmproxy/config/router_config.go
+++ nmproxy/config/router_config.go
@@ -210,14 +210,14 @@ func (c *Config) SetEgressRouterHandlers(inbound, outbound *pcap.Handle, cancel 
 func (c *Config) SetEgressBPFFilter() error {
 	c.Router.EgressRouter.mutex.Lock()
 	defer c.Router.EgressRouter.mutex.Unlock()
-	inBoundFilter := "dst net 10.235.166.1"
+	inBoundFilter := "dst host 10.235.166.1"
 	logger.Log(1, "Setting filters for egress inbound handler: ", inBoundFilter)
 	err := c.Router.EgressRouter.InboundHandler.SetBPFFilter(inBoundFilter)
 	if err != nil {
 		return errors.New("failed to set egress inbound bpf filter: " + err.Error())
 	}
 
-	outBoundFilter := "dst net 10.126.0.4"
+	outBoundFilter := "dst host 10.126.0.4"
 	logger.Log(1, "Setting filters for outbound handler: ", outBoundFilter)
 	err = c.Router.EgressRouter.OutBoundHandler.SetBPFFilter(outBoundFilter)
 	if err != nil {
