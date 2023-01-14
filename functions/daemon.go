3ec6db7da2da70a6c6d3aa877a0d77f04aebf856
0xdcarns <dillon.carns@gmail.com>
2023-01-14 13:51:10 -0500

    added host update

--- functions/daemon.go
+++ functions/daemon.go
@@ -247,11 +247,17 @@ func setupMQTTSingleton(server *config.Server) error {
 // should be called for each server host is registered on.
 func setHostSubscription(client mqtt.Client, server string) {
 	hostID := config.Netclient().ID
-	logger.Log(3, fmt.Sprintf("subscribed to host peer updates  peers/host/%s/%s", hostID.String(), server))
 	if token := client.Subscribe(fmt.Sprintf("peers/host/%s/%s", hostID.String(), server), 0, mqtt.MessageHandler(HostPeerUpdate)); token.Wait() && token.Error() != nil {
-		logger.Log(0, "MQ host sub: ", hostID.String(), token.Error().Error())
+		logger.Log(0, "MQ host peer sub: ", hostID.String(), token.Error().Error())
 		return
 	}
+	logger.Log(3, fmt.Sprintf("subscribed to host peer updates  peers/host/%s/%s", hostID.String(), server))
+
+	if token := client.Subscribe(fmt.Sprintf("host/update/%s/%s", hostID.String(), server), 0, mqtt.MessageHandler(HostPeerUpdate)); token.Wait() && token.Error() != nil {
+		logger.Log(0, "MQ host update sub: ", hostID.String(), server, token.Error().Error())
+		return
+	}
+	logger.Log(3, fmt.Sprintf("subscribed to host updates host/update/%s/%s", hostID.String(), server))
 }
 
 // setSubcriptions sets MQ client subscriptions for a specific node config
