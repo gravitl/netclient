f8cbeca150aaea016aa772f6b4372a37cba9c593
0xdcarns <dillon.carns@gmail.com>
2023-02-24 12:15:50 -0500

    remove lines

--- cmd/register.go
+++ cmd/register.go
@@ -7,23 +7,17 @@ import (
 	"github.com/gravitl/netclient/functions"
 	"github.com/gravitl/netmaker/logger"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // registerCmd represents the register command
 var registerCmd = &cobra.Command{
 	Use:   "register",
 	Short: "register to a Netmaker instance",
-
 	Long: `register to a Netmaker instance using: 
 token: netclient register -t <token> // join using an enrollment token`,
 	Run: func(cmd *cobra.Command, args []string) {
-		flags := viper.New()
-		flags.BindPFlags(cmd.Flags())
-		// CLI should always take password from stdin
-		flags.Set("readPassFromStdIn", true)
-		token := flags.GetString("token")
-		if token == "" {
+		token, err := cmd.Flags().GetString("token")
+		if err != nil {
 			cmd.Usage()
 			return
 		}
