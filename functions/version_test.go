cd340be1cd545a738023d745dbdc33a9b384fdf4
Matthew R Kasun <mkasun@nusak.ca>
2023-06-02 13:13:58 -0400

    set autoupdate on host update

--- functions/version_test.go
+++ functions/version_test.go
@@ -21,3 +21,19 @@ func TestIsVersionCompatible(t *testing.T) {
 		is.Equal(ok, true)
 	})
 }
+
+func TestVersionLessThan(t *testing.T) {
+	is := is.New(t)
+	t.Run("lower version", func(t *testing.T) {
+		ok := versionLessThan("v0.15.0", "v0.16.0")
+		is.Equal(ok, true)
+	})
+	t.Run("equal version", func(t *testing.T) {
+		ok := versionLessThan("v0.18.0", "v0.18.0")
+		is.Equal(ok, false)
+	})
+	t.Run("higher version", func(t *testing.T) {
+		ok := versionLessThan("v0.19.0", "v0.18.0")
+		is.Equal(ok, false)
+	})
+}
