1ca6d379c42f5ca1db0eba606baa1f66a3dd9d33
Abhishek Kondur <abhi281342@gmail.com>
2023-06-05 16:10:54 +0530

    switch command rename, add comments

--- cmd/server.go
+++ cmd/server.go
@@ -7,8 +7,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// installCmd represents the install command
-var setCtx = &cobra.Command{
+// switchServer command is used to set netclient context to a server, it has registered already.
+var switchServer = &cobra.Command{
 	Use:   "switch [ servername ]",
 	Short: "switch [ servername ]",
 	Long:  `sets  netclient to a server config`,
@@ -27,15 +27,5 @@ var setCtx = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(setCtx)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(switchServer)
 }
