c2f4d7f2f08b6012637b62ab5c2daa570c669f9f
Matthew R Kasun <mkasun@nusak.ca>
2023-06-22 07:19:40 -0400

    wireguard check on startup

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gravitl/netclient/config"
 	"github.com/gravitl/netclient/functions"
+	"github.com/gravitl/netclient/wireguard"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/exp/slog"
@@ -54,6 +55,11 @@ func initConfig() {
 	flags.BindPFlags(rootCmd.Flags())
 	config.InitConfig(flags)
 	setupLoging(flags)
+	nc := wireguard.NewNCIface(config.Netclient(), config.GetNodes())
+	if err := nc.Create(); err != nil {
+		slog.Error("failed to create interface, is wireguard installed", "error", err)
+		os.Exit(1)
+	}
 }
 
 func setupLoging(flags *viper.Viper) {
