2c53a264ab20fbd47588f60f4e98e8ba15df89a1
Abhishek Kondur <abhi281342@gmail.com>
2023-07-14 22:32:57 +0530

    rm exits on iface create error

--- functions/daemon.go
+++ functions/daemon.go
@@ -169,17 +169,9 @@ func startGoRoutines(wg *sync.WaitGroup) context.CancelFunc {
 	nc := wireguard.NewNCIface(config.Netclient(), config.GetNodes())
 	if err := nc.Create(); err != nil {
 		slog.Error("error creating netclient interface", "error", err)
-		os.Exit(1)
 	}
 	if err := nc.Configure(); err != nil {
 		slog.Error("error configuring netclient interface", "error", err)
-		// needed to prevent exit when there are no peers
-		if err.Error() == "ioctl: bad address" {
-			slog.Error("skipping bad error")
-		} else {
-			nc.Close()
-			os.Exit(1)
-		}
 	}
 	wireguard.SetPeers(true)
 	server := config.GetServer(config.CurrServer)
