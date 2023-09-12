c551add9c8a27e7214b409d990f069c639da3db5
Abhishek Kondur <abhi281342@gmail.com>
2023-09-12 15:35:26 +0530

    reset connection on negotation if peer conn is bad

--- nmproxy/turn/turn_helper.go
+++ nmproxy/turn/turn_helper.go
@@ -17,6 +17,7 @@ import (
 	"github.com/gravitl/netclient/wireguard"
 	"github.com/gravitl/netmaker/logger"
 	nm_models "github.com/gravitl/netmaker/models"
+	"golang.org/x/exp/slog"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
@@ -78,6 +79,16 @@ func handlePeerNegotiation(signal nm_models.Signal) error {
 				conn.Config.PeerEndpoint = peerTurnEndpoint
 				config.GetCfg().UpdatePeer(&conn)
 				config.GetCfg().ResetPeer(signal.FromHostPubKey)
+			} else if !signal.Reply {
+				// reset conn
+				connected, err := isPeerConnected(signal.FromHostPubKey)
+				if err != nil {
+					slog.Info("failed to check if peer is connected: ", err.Error())
+				}
+				if !connected {
+					config.GetCfg().ResetPeer(signal.FromHostPubKey)
+				}
+
 			}
 
 		} else {
