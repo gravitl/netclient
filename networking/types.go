b181e99744ab892b506e8d2daca57f6ffb1f2971
Farukh Khan <farukhkhan21@gmail.com>
2023-11-04 00:04:49 +0800

    Unused variables and types cleanup on types.go
    * Unused variables and types cleanup on types.go

--- networking/types.go
+++ networking/types.go
@@ -1,15 +1,5 @@
 package networking
 
-import (
-	"time"
-)
-
-// reqTimeout - five seconds for client request to happen
-const (
-	reqTimeout               = time.Second * 1
-	latencyVarianceThreshold = 3
-)
-
 // messages to hande client/server comms
 var messages = struct {
 	Wrong   string
@@ -17,11 +7,4 @@ var messages = struct {
 }{
 	Wrong:   "WRONG",
 	Success: "PONG",
-}
-
-// the type to send between client + server for local address detection
-type bestIfaceMsg struct {
-	Hash       string `json:"hash"`
-	TimeStamp  int64  `json:"time_stamp"`
-	ListenPort int    `json:"listenport"`
 }
