ea503189d466ae0b0725cc38f7f501f55690a454
Aceix <aceixsmartx@gmail.com>
2024-02-07 11:15:12 +0000

    fix(NET-732): userspace wireguard compatibility on linux

--- wireguard/wireguard_unix.go
+++ wireguard/wireguard_unix.go
@@ -5,8 +5,10 @@ package wireguard
 
 import (
 	"net"
+	"sync"
 
 	"github.com/gravitl/netclient/config"
+	"golang.org/x/exp/slog"
 	"golang.zx2c4.com/wireguard/conn"
 	"golang.zx2c4.com/wireguard/device"
 	"golang.zx2c4.com/wireguard/ipc"
@@ -14,6 +16,10 @@ import (
 )
 
 // == private ==
+
+var tunDevice *device.Device
+var wg sync.WaitGroup
+var uapi net.Listener
 
 func (nc *NCIface) createUserSpaceWG() error {
 	wgMutex.Lock()
@@ -24,22 +30,31 @@ func (nc *NCIface) createUserSpaceWG() error {
 		return err
 	}
 	nc.Iface = tunIface
-	tunDevice := device.NewDevice(tunIface, conn.NewDefaultBind(), device.NewLogger(device.LogLevelSilent, "[netclient] "))
+	tunDevice = device.NewDevice(tunIface, conn.NewDefaultBind(), device.NewLogger(device.LogLevelSilent, "[netclient] "))
 	err = tunDevice.Up()
 	if err != nil {
 		return err
 	}
-	uapi, err := getUAPIByInterface(nc.Name)
+	uapi, err = getUAPIByInterface(nc.Name)
 	if err != nil {
 		return err
 	}
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for {
-			uapiConn, uapiErr := uapi.Accept()
-			if uapiErr != nil {
-				continue
+			select {
+			case <-tunDevice.Wait():
+				slog.Debug("tunDevice.Wait() returned")
+				return
+			default:
+				uapiConn, uapiErr := uapi.Accept()
+				if uapiErr != nil {
+					slog.Debug("uapi error:", "error", uapiErr)
+					continue
+				}
+				go tunDevice.IpcHandle(uapiConn)
 			}
-			go tunDevice.IpcHandle(uapiConn)
 		}
 	}()
 	return nil
@@ -51,4 +66,18 @@ func getUAPIByInterface(iface string) (net.Listener, error) {
 		return nil, err
 	}
 	return ipc.UAPIListen(iface, tunSock)
+}
+
+func (nc *NCIface) closeUserspaceWg() error {
+	wgMutex.Lock()
+	defer wgMutex.Unlock()
+	slog.Debug("Closing userspace WireGuard interface", "interface", nc.Name)
+
+	tunDevice.Close()
+	uapi.Close()
+	wg.Wait()
+
+	slog.Debug("Closed userspace WireGuard interface", "interface", nc.Name)
+
+	return nil
 }
