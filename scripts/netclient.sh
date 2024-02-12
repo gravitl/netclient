d7a4c5bc5bebe4d4f1613bea4d4b1ed2eeea8547
liuwei <wei2460@qq.com>
2024-02-12 15:40:51 +0800

    fix: ENDPOINT env variable not working (#692)
    close #691

--- scripts/netclient.sh
+++ scripts/netclient.sh
@@ -31,8 +31,8 @@ if [ "${PORT}" != "" ]; then
 fi
 
 ENDPOINT_CMD=""
-if [ "${ENPOINT}" != "" ];then
-    ENDPOINT_CMD="-e ${ENPOINT}"
+if [ "${ENDPOINT}" != "" ];then
+    ENDPOINT_CMD="-e ${ENDPOINT}"
 fi
 
 MTU_CMD=""
