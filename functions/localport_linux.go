4da61f621b50d53876d2c1f9ea2b9a7bf6bbb1b8
Max Ma <mayabin@gmail.com>
2024-03-22 17:44:30 +0100

    fix getDefaultInterface ipv6 issue

--- functions/localport_linux.go
+++ functions/localport_linux.go
@@ -3,6 +3,7 @@ package functions
 import (
 	"errors"
 	"net"
+	"strings"
 
 	"github.com/vishvananda/netlink"
 )
@@ -12,7 +13,16 @@ func getDefaultInterface() (string, error) {
 	// routes[0] will be default route
 	routes, err := netlink.RouteGet(dest)
 	if err != nil {
-		return "", err
+		if !strings.Contains(err.Error(), "network is unreachable") {
+			return "", err
+		}
+		//if ipv4 address is unreachable, try ipv6 address
+		dest = net.ParseIP("2606:4700:4700::1111")
+		// routes[0] will be default route
+		routes, err = netlink.RouteGet(dest)
+		if err != nil {
+			return "", err
+		}
 	}
 	ifaces, err := net.Interfaces()
 	if err != nil {
