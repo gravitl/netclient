a8005e37a1b8c4666187d54069ed881427a66653
Max Ma <mayabin@gmail.com>
2024-10-10 01:34:16 -0900

    NET-1608: Update host to nm before restart if endpoint changed (#890)
    * increase peerConnectionCheck interval to 1 min
    
    * add HostUpdate before restart if endpoint changed
    
    * update config endpoint when there is endpoint change
    
    * set peers after pull in start
    
    * remove duplicate setpeers
    
    * add more log when endpoint changes
    
    * remove debug log

--- functions/mqpublish.go
+++ functions/mqpublish.go
@@ -83,14 +83,27 @@ func Checkin(ctx context.Context, wg *sync.WaitGroup) {
 				ip4, _, _ := holePunchWgPort(4, 0)
 				if ip4 != nil && !ip4.IsUnspecified() && !config.HostPublicIP.Equal(ip4) {
 					slog.Warn("IP CHECKIN", "ipv4", ip4, "HostPublicIP", config.HostPublicIP)
+					config.HostPublicIP = ip4
+					restart = true
+				} else if ip4 == nil && config.HostPublicIP != nil {
+					slog.Warn("IP CHECKIN", "ipv4", ip4, "HostPublicIP", config.HostPublicIP)
+					config.HostPublicIP = nil
 					restart = true
 				}
 				ip6, _, _ := holePunchWgPort(6, 0)
 				if ip6 != nil && !ip6.IsUnspecified() && !config.HostPublicIP6.Equal(ip6) {
 					slog.Warn("IP CHECKIN", "ipv6", ip6, "HostPublicIP6", config.HostPublicIP6)
+					config.HostPublicIP6 = ip6
+					restart = true
+				} else if ip6 == nil && config.HostPublicIP6 != nil {
+					slog.Warn("IP CHECKIN", "ipv6", ip6, "HostPublicIP6", config.HostPublicIP6)
+					config.HostPublicIP6 = nil
 					restart = true
 				}
 				if restart {
+					if err := UpdateHostSettings(true); err != nil {
+						slog.Warn("failed to update host settings", err.Error())
+					}
 					logger.Log(0, "restarting netclient due to network changes...")
 					daemon.HardRestart()
 				}
@@ -336,7 +349,7 @@ func UpdateHostSettings(fallback bool) error {
 	if !config.Netclient().IsStatic {
 		if config.HostPublicIP6 != nil && !config.HostPublicIP6.IsUnspecified() {
 			if !config.Netclient().EndpointIPv6.Equal(config.HostPublicIP6) {
-				logger.Log(0, "endpoint has changed from", config.Netclient().EndpointIPv6.String(), "to", config.HostPublicIP6.String())
+				logger.Log(0, "endpoint6 has changed from", config.Netclient().EndpointIPv6.String(), "to", config.HostPublicIP6.String())
 				config.Netclient().EndpointIPv6 = config.HostPublicIP6
 				publishMsg = true
 			}
