347c76a813c60d64fd73ccb08d546bb9c97f9e07
abhishek9686 <abhi281342@gmail.com>
2025-01-15 23:51:28 +0530

    collect metrics on peer update

--- functions/mqhandlers.go
+++ functions/mqhandlers.go
@@ -288,10 +288,13 @@ func HostPeerUpdate(client mqtt.Client, msg mqtt.Message) {
 	}
 
 	handleFwUpdate(serverName, &peerUpdate.FwUpdate)
-	go func() {
-		time.Sleep(time.Second * 15)
-		callPublishMetrics(true)
-	}()
+	if server.IsPro {
+		go func() {
+			time.Sleep(time.Second * 15)
+			callPublishMetrics(true)
+		}()
+	}
+
 }
 
 // HostUpdate - mq handler for host update host/update/<HOSTID>/<SERVERNAME>
