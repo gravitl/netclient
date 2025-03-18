4f0bc4a4c1ebd3c1f8958375c1d01b1a1e7fc0a3
Vishal Dalwadi <51291657+VishalDalwadi@users.noreply.github.com>
2025-03-17 22:37:53 -0700

    NET-1976: Add support for Managed DNS on Windows. (#999)
    * feat(go): prevent nxdomain errors;
    
    * feat(go): add managed dns support on windows;
    
    * fix(go): compile error;
    
    * fix(go): remove unused variables;
    
    * feat(go): return authoritative response;
    
    * feat(go): add split dns settings for windows;
    
    * fix(go): remove nrpt config;
    
    * fix(go): setup dns on windows when resetting interface;
    
    * fix(go): managed dns for multiple networks;

--- dns/config_windows.go
+++ dns/config_windows.go
@@ -1,16 +1,76 @@
 package dns
 
-func FlushLocalDnsCache() (err error) {
-	return nil
+import (
+	"fmt"
+	"github.com/gravitl/netclient/config"
+	"github.com/gravitl/netclient/ncutils"
+	"golang.org/x/exp/slog"
+	"golang.org/x/sys/windows/registry"
+	"net/netip"
+	"sync"
+)
+
+var dnsConfigMutex sync.Mutex
+
+func FlushLocalDnsCache() error {
+	_, err := ncutils.RunCmd("ipconfig /flushdns", false)
+	if err != nil {
+		slog.Warn("failed to flush local dns cache", "error", err.Error())
+	}
+
+	return err
 }
 
-func SetupDNSConfig() (err error) {
-	return nil
+func SetupDNSConfig() error {
+	dnsConfigMutex.Lock()
+	defer dnsConfigMutex.Unlock()
+
+	// ignore if dns manager is not Windows Registry
+	if config.Netclient().DNSManagerType != DNS_MANAGER_WINDOWS_REGISTRY {
+		return nil
+	}
+
+	dnsIp, err := getDnsIp()
+	if err != nil {
+		return err
+	}
+
+	ip, err := netip.ParseAddr(dnsIp)
+	if err != nil {
+		return err
+	}
+
+	guid := config.Netclient().Host.ID.String()
+	if guid == "" {
+		guid = config.DefaultHostID
+	}
+
+	guid = "{" + guid + "}"
+
+	keyPath := ""
+	if ip.Is6() {
+		keyPath = fmt.Sprintf(`SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\Interfaces\%s`, guid)
+	} else {
+		keyPath = fmt.Sprintf(`SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%s`, guid)
+	}
+
+	// open registry key with permissions to set value
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, keyPath, registry.QUERY_VALUE|registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	defer key.Close()
+
+	return key.SetStringValue("NameServer", dnsIp)
 }
 
-func RestoreDNSConfig() (err error) {
+func RestoreDNSConfig() error {
 	return nil
 }
 
 func InitDNSConfig() {
+	dnsConfigMutex.Lock()
+	defer dnsConfigMutex.Unlock()
+
+	config.Netclient().DNSManagerType = DNS_MANAGER_WINDOWS_REGISTRY
 }
