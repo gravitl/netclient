ff04a273193b4ce5bc0d4556d3ba039fe8c10e87
abhishek9686 <abhi281342@gmail.com>
2025-05-26 09:53:15 +0530

    remove pprof server

--- functions/daemon.go
+++ functions/daemon.go
@@ -9,9 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
@@ -50,12 +48,6 @@ var (
 	Mqclient     mqtt.Client
 	messageCache = new(sync.Map)
 )
-
-func init() {
-	go func() {
-		log.Println(http.ListenAndServe("127.0.0.1:6060", nil))
-	}()
-}
 
 type cachedMessage struct {
 	Message  string
