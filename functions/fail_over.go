53f0fc9a61d94c4575cb02f8e8b0daae22841aee
abhishek9686 <abhi281342@gmail.com>
2025-05-30 07:46:47 +0530

    ignore failover signals older than 3s

--- functions/fail_over.go
+++ functions/fail_over.go
@@ -36,7 +36,7 @@ func processPeerSignal(signal models.Signal) {
 
 	// process recieved new signal from peer
 	// if signal is older than 10s ignore it,wait for a fresh signal from peer
-	if time.Now().Unix()-signal.TimeStamp > 5 {
+	if time.Now().Unix()-signal.TimeStamp > 3 {
 		return
 	}
 	switch signal.Action {
