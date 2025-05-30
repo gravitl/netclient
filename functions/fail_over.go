58981484c73fc67b0f23f988cc534da3d4130cb7
Abhishek K <abhishek@netmaker.io>
2025-05-30 07:49:26 +0530

    NET-1932: reduce failover timeout on signals (#1033)
    * insert Fwd rules
    
    * fix fwd rules order
    
    * fix nftable fwd rule order
    
    * fix win build
    
    * remove bytes transmitted check
    
    * ignore failover signals older than 3s
    
    * ignore failover signals older than 3s

--- functions/fail_over.go
+++ functions/fail_over.go
@@ -35,8 +35,8 @@ var (
 func processPeerSignal(signal models.Signal) {
 
 	// process recieved new signal from peer
-	// if signal is older than 10s ignore it,wait for a fresh signal from peer
-	if time.Now().Unix()-signal.TimeStamp > 5 {
+	// if signal is older than 3s ignore it,wait for a fresh signal from peer
+	if time.Now().Unix()-signal.TimeStamp > 3 {
 		return
 	}
 	switch signal.Action {
