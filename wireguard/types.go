92c63de24d8bc4d9b16afa11c5aabcc0a165cf1a
abhishek9686 <abhi281342@gmail.com>
2025-06-26 08:20:35 +0530

    fix ipv6 egress ranges

--- wireguard/types.go
+++ wireguard/types.go
@@ -3,6 +3,7 @@ package wireguard
 import (
 	"fmt"
 	"net"
+	"runtime"
 	"sort"
 	"sync"
 
@@ -131,18 +132,27 @@ func SetEgressRoutes(egressRoutes []models.EgressNetworkRoutes) {
 			egressRangeIPNet := config.ToIPNet(egressRange.Network)
 			if egressRangeIPNet.IP != nil {
 				if len(config.GetNodes()) == 1 {
-
-					addrs = append(addrs, ifaceAddress{
-						Network: egressRangeIPNet,
-						GwIP:    egressRoute.EgressGwAddr.IP,
-						Metric:  egressRange.RouteMetric,
-					})
-
-					addrs = append(addrs, ifaceAddress{
-						Network: egressRangeIPNet,
-						GwIP:    egressRoute.EgressGwAddr6.IP,
-						Metric:  egressRange.RouteMetric,
-					})
+					if runtime.GOOS == "linux" {
+						addrs = append(addrs, ifaceAddress{
+							Network: egressRangeIPNet,
+							Metric:  egressRange.RouteMetric,
+						})
+					} else {
+						if egressRoute.EgressGwAddr.IP != nil {
+							addrs = append(addrs, ifaceAddress{
+								Network: egressRangeIPNet,
+								GwIP:    egressRoute.EgressGwAddr.IP,
+								Metric:  egressRange.RouteMetric,
+							})
+						}
+						if egressRoute.EgressGwAddr6.IP != nil {
+							addrs = append(addrs, ifaceAddress{
+								Network: egressRangeIPNet,
+								GwIP:    egressRoute.EgressGwAddr6.IP,
+								Metric:  egressRange.RouteMetric,
+							})
+						}
+					}
 
 					continue
 				}
