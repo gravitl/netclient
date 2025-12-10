68344ceabc94e0957120e76f8f3918a9e79e6fd9
VishalDalwadi <dalwadivishal26@gmail.com>
2025-12-10 22:10:32 +0530

    feat: add noop flow managers for windows and darwin;

--- flow/manager.go
+++ flow/manager.go
@@ -1,168 +1,25 @@
+//go:build !linux
+
 package flow
 
-import (
-	"crypto/tls"
-	"fmt"
-	"net"
-	"net/netip"
-	"runtime"
-	"sync"
-	"time"
+import "github.com/gravitl/netmaker/models"
 
-	"github.com/gravitl/netclient/config"
-	"github.com/gravitl/netclient/flow/exporter"
-	"github.com/gravitl/netclient/flow/tracker"
-	pbflow "github.com/gravitl/netmaker/grpc/flow"
-	"github.com/gravitl/netmaker/models"
-)
+type NoopManager struct{}
 
-const RefreshDuration = 10 * time.Minute
-
-type Manager struct {
-	participantIdentifiers map[string]models.PeerIdentity
-	flowClient             *exporter.FlowGrpcClient
-	flowTracker            *tracker.FlowTracker
-	startOnce              sync.Once
-	mu                     sync.RWMutex
-}
-
-var manager *Manager
+var manager *NoopManager
 
 func init() {
-	manager = &Manager{}
+	manager = &NoopManager{}
 }
 
-func GetManager() *Manager {
+func GetManager() *NoopManager {
 	return manager
 }
 
-func (m *Manager) Start(participantIdentifiers map[string]models.PeerIdentity) error {
-	if runtime.GOOS != "linux" {
-		return nil
-	}
-
-	m.mu.Lock()
-	m.participantIdentifiers = participantIdentifiers
-	m.mu.Unlock()
-
-	var err error
-	m.startOnce.Do(func() {
-		fmt.Println("[flow] starting flow manager")
-
-		flowClient := exporter.NewFlowGrpcClient(
-			config.GetServer(config.CurrServer).GRPC,
-			exporter.WithTLS(&tls.Config{}),
-		)
-
-		err = flowClient.Start()
-		if err != nil {
-			return
-		}
-
-		m.flowClient = flowClient
-
-		var flowTracker *tracker.FlowTracker
-		flowTracker, err = tracker.New(
-			func(f func(node *models.CommonNode) bool) {
-				for _, node := range config.GetNodes() {
-					if node.Server == config.CurrServer {
-						if !f(&node.CommonNode) {
-							return
-						}
-					}
-				}
-			},
-			func(addr netip.Addr) *pbflow.FlowParticipant {
-				ip := addr.String()
-				ipCidr := netip.PrefixFrom(addr, addr.BitLen()).String()
-
-				m.mu.RLock()
-				identity, found := m.participantIdentifiers[ipCidr]
-				if !found {
-					for addr := range m.participantIdentifiers {
-						_, cidr, err := net.ParseCIDR(addr)
-						if err != nil {
-							continue
-						}
-						if cidr.Contains(net.ParseIP(ip)) {
-							identity, found = m.participantIdentifiers[addr]
-							break
-						}
-					}
-				}
-				m.mu.RUnlock()
-				if !found {
-					return &pbflow.FlowParticipant{
-						Ip:   ip,
-						Type: pbflow.ParticipantType_PARTICIPANT_EXTERNAL,
-					}
-				}
-
-				participantType := pbflow.ParticipantType_PARTICIPANT_UNSPECIFIED
-				switch identity.Type {
-				case models.PeerType_Node:
-					participantType = pbflow.ParticipantType_PARTICIPANT_NODE
-				case models.PeerType_User:
-					participantType = pbflow.ParticipantType_PARTICIPANT_USER
-				case models.PeerType_WireGuard:
-					participantType = pbflow.ParticipantType_PARTICIPANT_EXTCLIENT
-				case models.PeerType_EgressRoute:
-					participantType = pbflow.ParticipantType_PARTICIPANT_EGRESS_ROUTE
-				}
-
-				return &pbflow.FlowParticipant{
-					Ip:   ip,
-					Type: participantType,
-					Id:   identity.ID,
-				}
-			},
-			flowClient,
-		)
-		if err != nil {
-			return
-		}
-
-		m.flowTracker = flowTracker
-
-		err = m.flowTracker.TrackConnections()
-		if err != nil {
-			return
-		}
-	})
-	if err != nil {
-		fmt.Println("[flow] error starting flow manager:", err)
-	}
-
-	return err
+func (m *NoopManager) Start(_ map[string]models.PeerIdentity) error {
+	return nil
 }
 
-func (m *Manager) Stop() error {
-	if runtime.GOOS != "linux" {
-		return nil
-	}
-
-	fmt.Println("[flow] stopping flow manager")
-
-	if m.flowClient != nil {
-		err := m.flowClient.Stop()
-		if err != nil {
-			fmt.Println("[flow] error stopping flow manager:", err)
-			return err
-		}
-		m.flowClient = nil
-	}
-
-	if m.flowTracker != nil {
-		err := m.flowTracker.Close()
-		if err != nil {
-			fmt.Println("[flow] error stopping flow manager:", err)
-			return err
-		}
-		m.flowTracker = nil
-	}
-
-	// reset start once
-	m.startOnce = sync.Once{}
-
+func (m *NoopManager) Stop() error {
 	return nil
 }
