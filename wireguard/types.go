281625ec900558da44a793d377e1e8c927792dad
abhishek9686 <abhi281342@gmail.com>
2025-12-16 21:35:50 +0400

    NM-177: add fw mark

--- wireguard/types.go
+++ wireguard/types.go
@@ -29,7 +29,7 @@ var wgMutex = sync.Mutex{} // used to mutex functions of the interface
 
 // NewNCIFace - creates a new Netclient interface in memory
 func NewNCIface(host *config.Config, nodes config.NodeMap) *NCIface {
-	firewallMark := 0
+	firewallMark := host.ListenPort
 	peers := config.Netclient().HostPeers
 	// on freebsd, calling wgcltl.Client.ConfigureDevice() with []Peers{} causes an ioctl error --> ioctl: bad address
 	if len(peers) == 0 {
