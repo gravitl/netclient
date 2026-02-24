bac788cafd95bf005f1a72e371a2cb494bc3e232
abhishek9686 <abhi281342@gmail.com>
2026-02-24 13:29:43 +0400

    NM-255: restore ip from server if nil on startup

--- functions/daemon.go
+++ functions/daemon.go
@@ -183,6 +183,15 @@ func startGoRoutines(wg *sync.WaitGroup) context.CancelFunc {
 	if err != nil {
 		logger.FatalLog("error initializing dns manager:", err.Error())
 	}
+	slog.Info("configuring netmaker wireguard interface")
+	var pullresp models.HostPull
+	var pullErr error
+	if server != nil && server.API != "" {
+		pullresp, _, _, pullErr = Pull(false, true)
+		if pullErr != nil {
+			slog.Error("fail to pull config from server", "error", pullErr.Error())
+		}
+	}
 
 	if !netclientCfg.IsStaticPort {
 		if freeport, err := ncutils.GetFreePort(ncutils.NetclientDefaultPort, netclientCfg.ListenPort, false); err != nil {
@@ -207,8 +216,10 @@ func startGoRoutines(wg *sync.WaitGroup) context.CancelFunc {
 			updateConfig = true
 		} else {
 			slog.Warn("GetPublicIPv4 error:", "Warn", "no ipv4 found")
-			netclientCfg.EndpointIP = nil
-			updateConfig = true
+			if netclientCfg.EndpointIP != nil {
+				config.HostPublicIP = netclientCfg.EndpointIP
+				slog.Info("seeded HostPublicIP from stored endpoint", "ip", netclientCfg.EndpointIP)
+			}
 		}
 		if netclientCfg.NatType == "" {
 			netclientCfg.NatType = config.HostNatType
@@ -226,8 +237,10 @@ func startGoRoutines(wg *sync.WaitGroup) context.CancelFunc {
 			updateConfig = true
 		} else {
 			slog.Warn("GetPublicIPv6 Warn: ", "Warn", "no ipv6 found")
-			netclientCfg.EndpointIPv6 = nil
-			updateConfig = true
+			if netclientCfg.EndpointIPv6 != nil {
+				config.HostPublicIP6 = netclientCfg.EndpointIPv6
+				slog.Info("seeded HostPublicIP6 from stored endpoint", "ip", netclientCfg.EndpointIPv6)
+			}
 		}
 		if netclientCfg.WgPublicListenPort != config.WgPublicListenPort {
 			netclientCfg.WgPublicListenPort = config.WgPublicListenPort
@@ -248,11 +261,7 @@ func startGoRoutines(wg *sync.WaitGroup) context.CancelFunc {
 			slog.Warn("error writing endpoint/port netclient config file", "error", err)
 		}
 	}
-	slog.Info("configuring netmaker wireguard interface")
-	pullresp, _, _, pullErr := Pull(false, true)
-	if pullErr != nil {
-		slog.Error("fail to pull config from server", "error", pullErr.Error())
-	}
+
 	nc := wireguard.NewNCIface(netclientCfg, config.GetNodes())
 	if err := nc.Create(); err != nil {
 		slog.Error("error creating netclient interface", "error", err)
@@ -675,7 +684,7 @@ func UpdateKeys() error {
 func holePunchWgPort(proto, portToStun int) (pubIP net.IP, pubPort int, natType string) {
 	defer func() {
 		//ncutils.TraceCaller()
-		slog.Debug("holePunchWgPort", "proto", proto, "PortToStun", portToStun, "PubIP", pubIP.String(), "PubPort", pubPort, "NatType", natType)
+		slog.Error("holePunchWgPort", "proto", proto, "PortToStun", portToStun, "PubIP", pubIP.String(), "PubPort", pubPort, "NatType", natType)
 	}()
 	server := config.GetServer(config.CurrServer)
 	if server == nil {
